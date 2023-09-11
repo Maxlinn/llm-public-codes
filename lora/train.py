c606861195f6d03b14b71a749699f72ffda0c6b1
Maxlinn <maxlinn@foxmail.com>
2023-09-11 16:19:40 +0800

    fix typo CAUSAL_LM

--- lora/train.py
+++ lora/train.py
@@ -30,7 +30,7 @@ class MyArguments:
 # :add:
 @dataclass
 class LoraArguments:
-    lora_task_type :str = "CASUAL_LM"
+    lora_task_type :str = "CAUSAL_LM"
     lora_r :int = 8
     lora_alpha :int = 32
     lora_dropout :float = 0.1
